1326686da2cceaedd602409ea948768e633273ab
alexonbstudio <alexonbstudio@gmail.com>
2020-07-09 08:40:14 +0200

    Update composer.sh

--- composer.sh
+++ composer.sh
@@ -2,18 +2,9 @@
 
 clear
 # preparing
+sudo -i
+mkdir -p tmp && cd /tmp
 
-if [ ! -x compser ]; then
-	curl -O https://github.com/website-project-WP/composer-wp/releases/download/1.0-composer/composer-wp-1.0RC.tar.gz
-	tar -xzvf composer-wp-1.0RC.tar.gz
-	chmod +x *.sh
-	bash ./install-composer.sh
-	bash ./update-composer.sh
-	bash ./require-source.sh
-#	bash ./advanced-require-source.sh
-	rm -f composer-wp-1.0RC.tar.gz
-	rm -f *.sh
-fi 
 
 	curl -O https://github.com/website-project-WP/composer-wp/releases/download/1.0-composer/composer-wp-1.0RC.tar.gz
 	tar -xzvf composer-wp-1.0RC.tar.gz
@@ -22,5 +13,5 @@ fi
 	bash ./update-composer.sh
 	bash ./require-source.sh
 #	bash ./advanced-require-source.sh
-	rm -f composer-wp-1.0RC.tar.gz
-	rm -f *.sh
+	rm -rf composer-wp-1.0RC.tar.gz
+	rm -rf *.sh
